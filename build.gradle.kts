bd568d3cf07a7270cbe6bfe628b2381ad8651437
Fred Silva <fred.silva@avanceesoftware.com>
2023-03-30 11:28:48 +0100

    Updated multiplatform version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application").version("7.4.2").apply(false)
     id("com.android.library").version("7.4.2").apply(false)
     kotlin("android").version("1.8.0").apply(false)
-    kotlin("multiplatform").version("1.8.20-Beta").apply(false)
+    kotlin("multiplatform").version("1.8.20-RC2").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
