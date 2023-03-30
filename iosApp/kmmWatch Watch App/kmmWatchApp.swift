fbd7f6ad932f628701e3bd1e29099796c8bec12a
Fred Silva <fred.silva@avanceesoftware.com>
2023-03-30 11:09:43 +0100

    Added LibraryKit to watch

---
+++ iosApp/kmmWatch Watch App/kmmWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  kmmWatchApp.swift
+//  kmmWatch Watch App
+//
+//  Created by Fred Silva on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct kmmWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
